68e040400898cb9a0e53805a41724f8446f0f929
kennyhn <kennyhn@stud.ntnu.no>
2019-02-05 12:39:27 +0100

    Funker og bedre enn i stad, men nty

--- orders.c
+++ orders.c
@@ -66,45 +66,6 @@ void orders_remove_all(){
 }
 
 
-
-
-/* -------------------------------------- */
-
-
-bool order_on_floor(int floor){
-    order_t* ptr = head;
-    while (ptr != NULL){
-        if (ptr->floor == floor){
-            return true;
-        }
-        ptr = ptr->next;
-    }
-    return false;
-}
-
-
-static bool orders_above(int floor){
-    order_t* ptr = head;
-    while (ptr != NULL){
-        if (ptr->floor > floor){
-            return true;
-        }
-        ptr = ptr->next;
-    }
-    return false;
-}
-
-static bool orders_below(int floor){
-    order_t* ptr = head;
-    while (ptr != NULL){
-        if (ptr->floor < floor){
-            return true;
-        }
-        ptr = ptr->next;
-    }
-    return false;
-}
-
 dir_t order_get_direction_of_order(int floor){
     order_t* ptr = head;
     while (ptr != NULL){
@@ -113,51 +74,31 @@ dir_t order_get_direction_of_order(int floor){
         }
         ptr = ptr->next;
     }
-    return 0;
+    return 2;
 }
+
 
 bool order_is_executable_on_floor(int floor, dir_t current_dir){   
-    if (order_on_floor(floor)){
-
-        
-
-
-        dir_t ord_dir = order_get_direction(floor);
+    int ord_dir = order_get_direction_of_order(floor);
+    if (ord_dir == 2){
+        return false; 
+    }
+    else 
+    {  
         if (ord_dir == current_dir || ord_dir == DIR_NEUTRAL){
-            return true;
+            return true; 
         }
-        if (ord_dir ==  DIR_UP && !orders_below(floor)){
-            return true;
-        }
-        if (ord_dir == DIR_DOWN && !orders_above(floor)){
-            return true;
-        }
-    } 
-    return false;
-}
 
-bool order_is_executable_on_floor(int floor, dir_t current_dir){
-/*
-    check if order exists for floor
-    get direction of order that exists on the floor
-    
-    if direction of order is neutral and direction of elevator is neutral return true
-    if direction of order is up and there are no orders below floor, return true
-    if direction of order is down and there are no orders above floor, return true
-    
-*/
-
-    if(order_on_floor(floor)){
         order_t* ptr = head;
         while (ptr != NULL){
-            if 
-
+            if ((ptr->floor < floor && ord_dir == DIR_DOWN) || (ptr->floor > floor && ord_dir == DIR_UP)){
+                return true;
+            }
             ptr = ptr->next;
         }
     }
-
-/* -------------------------------------- */
-
+    return false;
+}
 
 
 bool orders_is_empty(){
